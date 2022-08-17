0e34826eb01bc56db627f88542ab9848e195ee55
Viktor Kozma <kozmaviktor1992@gmail.com>
2022-08-17 21:51:42 +0800

    With serious styling uplift for index and show pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,14 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+Flat.destroy_all
+
 Flat.create!(
   name: 'Light & Spacious Garden Flat London',
   address: '10 Clifton Gardens London W9 1DT',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
   price_per_night: 75,
   number_of_guests: 3
-)
-
-Flat.create!(
-  name: 'Flat with gorgeus view in Budapest',
-  address: '19 Thokoly street Budapest 1076',
-  description: 'Truly metropolitan flat for active young people in the middle of the city with the best park in close proximity',
-  price_per_night: 35,
-  number_of_guests: 4
 )
 
 Flat.create!(
